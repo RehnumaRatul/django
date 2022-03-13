8b262e0690e28b5fd3bb60a093302383a3ecaea3
RehnumaRatul <rratul163047@bscse.uiu.ac.bd>
2022-03-13 13:06:53 +0600

    first commit

---
+++ basic/models.py
@@ -0,0 +1,13 @@
+from tkinter import Image
+from django.db import models
+
+# Create your models here.
+
+
+
+class Post(models.Model):
+    title = models.TextField()
+    cover = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
